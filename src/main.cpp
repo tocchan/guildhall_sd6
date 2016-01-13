383920e6a84e2759815685555faa9aa92913aff4
Chris Forseth <christopher.forseth@gmail.com>
2016-01-13 08:40:12 -0600

    Working client / server connection.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+char const *gPort = "5413";
+
+
 //-------------------------------------------------------------------------------------------------------
 std::string WindowsErrorAsString( DWORD error_id ) 
 {
@@ -61,6 +64,7 @@ void* GetInAddr(sockaddr *sa)
     }
 }
 
+//-------------------------------------------------------------------------------------------------------
 char* AllocHostName()
 {
    // from docs, 256 is max namelen allowed.
@@ -87,6 +91,10 @@ SOCKET BindAddress( char const *ip, char const *port, int af_family = AF_UNSPEC,
    addrinfo hints;
    addrinfo *addr;
 
+   if (ip == nullptr) {
+      ip = "localhost";
+   }
+
    memset( &hints, 0, sizeof(hints) );
    hints.ai_family   = af_family;       // if it matters, us AF_INET for IPv4, and AF_INET6 for IPv6
    hints.ai_socktype = type;
@@ -94,7 +102,7 @@ SOCKET BindAddress( char const *ip, char const *port, int af_family = AF_UNSPEC,
 
    // helper method - removes a lot of manual setup or sockaddr construction
    // but will also allocate on the heap on success, so will need to be freed.
-   int status = getaddrinfo( "localhost", port, &hints, &addr );
+   int status = getaddrinfo( ip, port, &hints, &addr );
    if (status != 0) {
       printf( "Failed to create socket address: %s\n", gai_strerror(status) );
    } else {
@@ -104,10 +112,9 @@ SOCKET BindAddress( char const *ip, char const *port, int af_family = AF_UNSPEC,
       for (iter = addr; iter != nullptr; iter = iter->ai_next) {
          char addr_name[INET6_ADDRSTRLEN];
          inet_ntop( iter->ai_family, GetInAddr(iter->ai_addr), addr_name, INET6_ADDRSTRLEN );
-         printf( "Attempt to bind on: %s\n", addr_name );
+         printf( "Attempt to bind on: %s : %s\n", addr_name, port );
          
          host_sock = socket( iter->ai_family, iter->ai_socktype, iter->ai_protocol );
-         
 
          if (host_sock != INVALID_SOCKET) {
             if (bind( host_sock, iter->ai_addr, (int)(iter->ai_addrlen) ) == SOCKET_ERROR) {
@@ -128,11 +135,12 @@ SOCKET BindAddress( char const *ip, char const *port, int af_family = AF_UNSPEC,
    return host_sock;
 }
 
+
 //-------------------------------------------------------------------------------------------------------
-void NetworkHost()
+void NetworkHost( char const *port )
 {
    char *host_name = AllocHostName();
-   SOCKET host = BindAddress( host_name, "3490", AF_INET );
+   SOCKET host = BindAddress( "localhost", port, AF_INET );
    free(host_name);
    if (host == INVALID_SOCKET) {
       printf( "Failed to create listen socket.\n" );
@@ -170,14 +178,70 @@ void NetworkHost()
          int len = recv( sock_them, buffer, 2048, 0 );
          buffer[len] = 0;
          printf( "Got message: %s\n", buffer );
+         send( sock_them, "pong", 4, 0 ); 
          closesocket(sock_them);
       }
     }
 }
 
 //-------------------------------------------------------------------------------------------------------
-void NetworkJoin( char const *buf )
+void NetworkJoin( char const *addrname, char const *port, char const *msg )
 {
+   SOCKET host_sock = INVALID_SOCKET;
+   addrinfo hints;
+   addrinfo *addr;
+       
+   memset( &hints, 0, sizeof(hints) );
+   hints.ai_family   = AF_UNSPEC;       // if it matters, us AF_INET for IPv4, and AF_INET6 for IPv6
+   hints.ai_socktype = SOCK_STREAM;  
+   hints.ai_flags    = AI_PASSIVE;      // Is used for binding (ie, listening)
+
+   // helper method - removes a lot of manual setup or sockaddr construction
+   // but will also allocate on the heap on success, so will need to be freed.
+   int status = getaddrinfo( addrname, port, &hints, &addr );
+   if (status != 0) {
+      printf( "Failed to create socket address: %s\n", gai_strerror(status) );
+   } else {
+      // Alright, walk the list, and bind when able
+      addrinfo *iter;
+      
+      for (iter = addr; iter != nullptr; iter = iter->ai_next) {
+         char addr_name[INET6_ADDRSTRLEN];
+         inet_ntop( iter->ai_family, GetInAddr(iter->ai_addr), addr_name, INET6_ADDRSTRLEN );
+         printf( "Attempt to bind on: %s\n", addr_name );
+         
+         host_sock = socket( iter->ai_family, iter->ai_socktype, iter->ai_protocol );
+
+         if (host_sock != INVALID_SOCKET) {
+            if (connect( host_sock, iter->ai_addr, iter->ai_addrlen ) == SOCKET_ERROR) {
+               closesocket( host_sock );
+               host_sock = INVALID_SOCKET;
+            } else {
+               // Connecting on address 
+               printf( "Connected to : %s\n", addr_name );
+               break;
+            }
+         }
+      }
+
+      // We're dont with the address, clean up my memory
+      freeaddrinfo(addr);
+   }
+
+   if (host_sock != INVALID_SOCKET) {
+      send( host_sock, msg, strlen(msg), 0 ); 
+
+      char buffer[128];
+      int len = recv( host_sock, buffer, 128, 0 );
+      if (len > 0) {
+         buffer[len] = NULL;
+         printf( "Received response: %s\n", buffer );
+      }
+   } else {
+      printf( "Could not connect...\n" );
+   }
+
+   closesocket( host_sock );
 }
 
 //-------------------------------------------------------------------------------------------------------
@@ -189,23 +253,26 @@ int main( int argc, char const **argv )
       return false;
    }
 
-   if (argc > 0) {
-      char const *arg = argv[0];
-      printf( "Arg: %s\n", arg );
-
-      if (_strcmpi( arg, "host" )) {
-         NetworkHost(); 
+   if (argc > 1) {                                               
+      if (_strcmpi( argv[1], "host" ) == 0) {
+         printf( "Hosting...\n" );
+         NetworkHost( gPort ); 
+      } else if (argc > 2) {
+         char const *addr = argv[1];
+         char const *msg = argv[2];
+         printf( "Sending message \"%s\" to [%s]\n", msg, addr );
+         NetworkJoin( addr, gPort, msg );
       } else {
-         NetworkJoin( arg );
+         printf( "Either past \"host\" or \"<addr> <msg>\"\n" );
       }
 
    } else {
       printf( "Please pass in a name.\n" );
-      _getch();
    }
 
    NetworkDeinit();
 
+   printf( "Press any key to continue...\n" );
    _getch();
    return 0;
 }
